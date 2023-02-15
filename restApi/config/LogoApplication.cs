e506eb0a2f8e4783ecb52ed17c44a7b8c77fd61a
Mücahit Ceviz <invalid.10bjects.com>
2023-02-15 20:06:02 +0300

    some change

---
+++ restApi/config/LogoApplication.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityObjects;
+
+namespace restApi.config
+{
+    internal class LogoApplication
+    {
+        private static UnityApplication unityApplication { get; set; }
+
+        public static UnityApplication getApplication()
+        {
+            if (unityApplication == null)
+                unityApplication = new UnityApplication();
+            return unityApplication;
+        }
+    }
+}
--- restApi/config/configuration.cs
+++ restApi/config/configuration.cs
@@ -29,97 +29,17 @@ namespace restApi.config
         {
             return ConfigurationManager.AppSettings["logoConnection"];
         }
-        public static Int32 getDistributorId()
-        {
-            return Convert.ToInt32(ConfigurationManager.AppSettings["DistributorId"]);
-        }
-        public static bool getTransferCreditCartToCase()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["TransferCreditCartToCase"]);
-        }
-        public static bool getUseCypheCode()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["useCypheCode"]);
-        }
-        public static bool getUseInvoice()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["useInvoice"]);
-        }
-        public static bool getUseDispatch()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["useDispatch"]);
-        }
-        public static bool getUseCollection()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["useCollection"]);
-        }
-        public static string getUnit(string unit)
-        {
-            return ConfigurationManager.AppSettings[unit];
-        }
         public static string getDepartment()
         {
             return ConfigurationManager.AppSettings["Department"];
-        }
-        public static bool getIsProducerCode()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["isProducerCode"]);
-        }
-        public static bool getIsBarcode()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["isBarkod"]);
-        }
-        public static bool getUseDefaultNumber()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["useDefaultNumber"]);
-        }
-        public static bool getUseShortDate()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["useShortDate"]);
-        }
-        public static bool getUseShipCode()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["useShipCode"]);
         }
         public static bool getXMLTransferInfo()
         {
             return Convert.ToBoolean(ConfigurationManager.AppSettings["XMLTransfer"]);
         }
-        public static bool getXMLTransferForOrder()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["XMLTransferForOrder"]);
-        }
-        public static string getCashCode()
-        {
-            return ConfigurationManager.AppSettings["CashCode"];
-        }
-        public static string getCypheCode()
-        {
-            return ConfigurationManager.AppSettings["YetkiKodu"];
-        }
         public static string getUrl()
         {
             return ConfigurationManager.AppSettings["URL"];
-        }
-        public static bool getIntegrationForMikroERP()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["integrationForMikroERP"]);
-        }
-        public static string getShipAgentCode()
-        {
-            return ConfigurationManager.AppSettings["shipAgentCode"];
-        }
-        public static string getCampaignLineNo()
-        {
-            return ConfigurationManager.AppSettings["campaignLineNo"];
-        }
-        public static bool getOrderTransferToLogoInfo()
-        {
-            return Convert.ToBoolean(ConfigurationManager.AppSettings["orderTransferToLogo"]);
-        }
-        public static string getPaymentCode()
-        {
-            return ConfigurationManager.AppSettings["PaymentCode"];
         }
     }
 }
